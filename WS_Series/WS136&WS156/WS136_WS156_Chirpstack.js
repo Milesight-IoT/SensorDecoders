abd3ce9dceae58ecda2fb83bd3c62008d7b9e60d
Milesight IoT <75722092+Milesight-IoT@users.noreply.github.com>
2022-05-18 17:16:35 +0800

    WS136&WS156 Decoder

---
+++ WS_Series/WS136&WS156/WS136_WS156_Chirpstack.js
@@ -0,0 +1,34 @@
+/**
+ * Payload Decoder for The Things Network
+ *
+ * Copyright 2022 Milesight IoT
+ *
+ * @product WS136 & WS156
+ */
+function Decode(fPort, bytes) {
+    var decoded = {};
+
+    for (var i = 0; i < bytes.length; ) {
+        var channel_id = bytes[i++];
+        var channel_type = bytes[i++];
+        // BATTERY
+        if (channel_id === 0x01 && channel_type === 0x75) {
+            decoded.battery = bytes[i];
+            i += 1;
+        }
+        // PRESS STATE
+        else if (channel_id === 0xff && channel_type === 0x34) {
+            var id = bytes[i];
+            var command = [bytes[i + 2], bytes[i + 1]];
+            var channel_name = "channel_" + id;
+            var channel_command_name = "channel_" + id + "_command";
+            decoded[channel_name] = "trigger";
+            decoded[channel_command_name] = command;
+            i += 3;
+        } else {
+            break;
+        }
+    }
+
+    return decoded;
+}
---
+++ WS_Series/WS136&WS156/WS136_WS156_TTN.js
@@ -0,0 +1,32 @@
+/**
+ * Payload Decoder for The Things Network
+ *
+ * Copyright 2022 Milesight IoT
+ *
+ * @product WS136 / WS156
+ */
+function Decoder(bytes, port) {
+    var decoded = {};
+
+    for (var i = 0; i < bytes.length; ) {
+        var channel_id = bytes[i++];
+        var channel_type = bytes[i++];
+        // BATTERY
+        if (channel_id === 0x01 && channel_type === 0x75) {
+            decoded.battery = bytes[i];
+            i += 1;
+        }
+        // PRESS STATE
+        else if (channel_id === 0xff && channel_type === 0x34) {
+            var id = bytes[i];
+            var command = [bytes[i + 2], bytes[i + 1]];
+            decoded[`channel_${id}`] = "trigger";
+            decoded[`channel_${id}_command`] = command;
+            i += 3;
+        } else {
+            break;
+        }
+    }
+
+    return decoded;
+}
