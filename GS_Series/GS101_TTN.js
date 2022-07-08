5a6e235f438b244ea6007c0372471266aeebcf2a
Milesight IoT <75722092+Milesight-IoT@users.noreply.github.com>
2022-07-08 12:48:10 +0800

    Create GS101_TTN.js

---
+++ GS_Series/GS101_TTN.js
@@ -0,0 +1,70 @@
+function Decoder(bytes, port) {
+    var decoded = {};
+
+    for (var i = 0; i < bytes.length;) {
+        var channel_id = bytes[i++];
+        var channel_type = bytes[i++];
+        //gas status
+        if (channel_id === 0x05 && channel_type === 0x8e) {
+            decoded.state= (bytes[i] === 0) ? 'normal' : 'abnormal';
+            i += 1;
+        }
+		    //vale
+        else if (channel_id === 0x06 && channel_type === 0x01) {
+            decoded.valve = (bytes[i] === 0) ? 'close' : 'open';
+            i += 1;
+        }
+        //relay
+        else if (channel_id === 0x07 && channel_type === 0x01) {
+            decoded.relay = (bytes[i] === 0) ? 'close' : 'open';
+            i += 1;
+        }
+        //remained life time for the sensor
+		    else if (channel_id === 0x08 && channel_type === 0x90) {
+            decoded.life_remain = readUInt32LE(bytes.slice(i, i + 4)) + "s";
+            i += 4;
+        }
+        //alarm info
+		    else if (channel_id === 0xff && channel_type === 0x3f) {
+            var alarm_type = bytes[i];
+            switch (alarm_type) {
+                case 0:
+                    decoded.alarm = 'power down';
+                    i += 1;
+                    break;
+                case 1:
+                    decoded.alarm = 'power on';
+                    i += 1;
+                    break;
+                case 2:
+					          decoded.alarm = 'sensor failure';
+                    i += 1;
+                    break;
+                case 3:
+                    decoded.alarm = 'sensor recover';
+                    i += 1;
+                    break;
+                case 4:
+                    decoded.alarm = 'sensor about to fail';
+                    i += 1;
+                    break;
+                case 5:
+                    decoded.alarm = 'sensor failed';
+                    i += 1;
+                    break;
+            }
+        }else {
+            break;
+        }
+    }
+
+    return decoded;
+}
+
+/* ******************************************
+ * bytes to number
+ ********************************************/
+function readUInt32LE(bytes) {
+    var value = (bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0];
+    return value & 0xffffffff;
+}
