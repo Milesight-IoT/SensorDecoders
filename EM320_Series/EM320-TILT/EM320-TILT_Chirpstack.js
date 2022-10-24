b9ead32d55894c20c963352e1bd13f547ec2aa8a
Sway <sway@milesight.com>
2022-10-24 19:19:10 +0800

    feat(em320-tilt): Support tilt sensor

---
+++ EM320_Series/EM320-TILT/EM320-TILT_Chirpstack.js
@@ -0,0 +1,48 @@
+/**
+ * Payload Decoder for Chirpstack and Milesight network server
+ *
+ * Copyright 2022 Milesight IoT
+ *
+ * @product EM320-TILT
+ */
+ function Decode(fPort, bytes) {
+    var decoded = {};
+
+    for (var i = 0; i < bytes.length; ) {
+        var channel_id = bytes[i++];
+        var channel_type = bytes[i++];
+
+        // BATTERY
+        if (channel_id === 0x01 && channel_type === 0x75) {
+            decoded.battery = bytes[i];
+            i += 1;
+        }
+        // ANGLE
+        else if (channel_id === 0x03 && channel_type === 0xD4) {
+            decoded.angle_x = (readInt16LE(bytes.slice(i, i + 2)) >> 1) / 100;
+            decoded.angle_y = (readInt16LE(bytes.slice(i + 2, i + 4)) >> 1) / 100;
+            decoded.angle_z = (readInt16LE(bytes.slice(i + 4, i + 6)) >> 1) / 100;
+            decoded.threhold_x = (bytes[i] & 0x01) === 0x01 ? "trigger" : "normal";
+            decoded.threhold_y = (bytes[i + 2] & 0x01) === 0x01 ? "trigger" : "normal";
+            decoded.threhold_z = (bytes[i + 4] & 0x01) === 0x01 ? "trigger" : "normal";
+            i += 6;
+        } else {
+            break;
+        }
+    }
+
+    return decoded;
+}
+
+/* ******************************************
+ * bytes to number
+ ********************************************/
+function readUInt16LE(bytes) {
+    var value = (bytes[1] << 8) + bytes[0];
+    return value & 0xffff;
+}
+
+function readInt16LE(bytes) {
+    var ref = readUInt16LE(bytes);
+    return ref > 0x7fff ? ref - 0x10000 : ref;
+}
