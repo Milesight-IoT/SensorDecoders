09ad58ff825a2656bed8d8430658efc93f831e80
Milesight IoT <75722092+Milesight-IoT@users.noreply.github.com>
2022-10-31 19:42:22 +0800

    Update WS50x_2_uplink_TTN.js

--- WS_Series/WS50x/WS50x_2_uplink_TTN.js
+++ WS_Series/WS50x/WS50x_2_uplink_TTN.js
@@ -59,3 +59,23 @@ function Decoder(bytes, port) {
 
     return decoded;
 }
+
+function readUInt16LE(bytes) {
+    var value = (bytes[1] << 8) + bytes[0];
+    return value & 0xffff;
+}
+
+function readInt16LE(bytes) {
+    var ref = readUInt16LE(bytes);
+    return ref > 0x7fff ? ref - 0x10000 : ref;
+}
+
+function readUInt32LE(bytes) {
+    var value = (bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0];
+    return value & 0xffffffff;
+}
+
+function readInt32LE(bytes) {
+    var ref = readUInt32LE(bytes);
+    return ref > 0x7fffffff ? ref - 0x100000000 : ref;
+}
