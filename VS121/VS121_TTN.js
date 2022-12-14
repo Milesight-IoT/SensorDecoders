bf11d5424e0d53f480581a69223c0ebc74842590
洪诗伟 <sway@milesight.com>
2022-12-14 20:09:54 +0800

    feat(vs121): support region count

--- VS121/VS121_TTN.js
+++ VS121/VS121_TTN.js
@@ -8,7 +8,7 @@
 function Decoder(bytes, port) {
     var decoded = {};
 
-    for (i = 0; i < bytes.length; ) {
+    for (i = 0; i < bytes.length;) {
         var channel_id = bytes[i++];
         var channel_type = bytes[i++];
 
@@ -44,15 +44,27 @@ function Decoder(bytes, port) {
             i += 4;
         }
         // PEOPLE IN/OUT
-        else if (channel_id ===0x05 && channel_type === 0xcc) {
+        else if (channel_id === 0x05 && channel_type === 0xcc) {
             decoded.in = readInt16LE(bytes.slice(i, i + 2));
             decoded.out = readInt16LE(bytes.slice(i + 2, i + 4));
             i += 4;
-        } 
+        }
         // PEOPLE MAX
-        else if (channel_id ===0x06 && channel_type === 0xcd) {
+        else if (channel_id === 0x06 && channel_type === 0xcd) {
             decoded.people_max = bytes[i];
             i += 1;
+        }
+        // REGION COUNTER
+        else if (channel_id === 0x07 && channel_type === 0xd5) {
+            decoded.region_1_counter = bytes[i];
+            decoded.region_2_counter = bytes[i + 1];
+            decoded.region_3_counter = bytes[i + 2];
+            decoded.region_4_counter = bytes[i + 3];
+            decoded.region_5_counter = bytes[i + 4];
+            decoded.region_6_counter = bytes[i + 5];
+            decoded.region_7_counter = bytes[i + 6];
+            decoded.region_8_counter = bytes[i + 7];
+            i += 8;
         } else {
             break;
         }
