8071c4b7ee3dd07eb870322e321728da29571a5b
Sway <sway@milesight.com>
2023-03-04 23:01:12 +0800

    feat(ts101): Insertion Temperature Sensor

---
+++ TS_Series/TS101/README.md
@@ -0,0 +1,40 @@
+# Insertion Temperature Sensor - Milesight IoT
+
+![TS101](TS101.png)
+
+The payload decoder function is applicable to TS101.
+
+For more detailed information, please visit [milesight official website](https://www.milesight-iot.com).
+
+## Payload Definition
+
+```
+--------------------- Payload Definition ---------------------
+
+                   [channel_id] [channel_type] [channel_value]
+01: battery      -> 0x01         0x75          [1byte ] Unit: %
+03: temperature  -> 0x03         0x67          [2bytes] Unit: °C (°F)
+------------------------------------------ TS101
+```
+
+## Example for The Things Network
+
+**Payload**
+
+```
+01 75 64 03 67 07 01
+```
+
+**Data Segmentation**
+
+-   `01 75 64`
+-   `03 67 07 01`
+
+**Output**
+
+```json
+{
+    "battery": 100,
+    "temperature": 26.3
+}
+```
---
+++ TS_Series/TS101/TS101_Chirpstack.js
@@ -0,0 +1,85 @@
+/**
+ * Payload Decoder for Chirpstack and Milesight network server
+ *
+ * Copyright 2023 Milesight IoT
+ *
+ * @product TS101
+ */
+function Decode(fPort, bytes) {
+    return milesight(bytes);
+}
+
+function milesight(bytes) {
+    var decoded = {};
+
+    for (var i = 0; i < bytes.length; ) {
+        var channel_id = bytes[i++];
+        var channel_type = bytes[i++];
+        // battery
+        if (channel_id === 0x01 && channel_type === 0x75) {
+            decoded.battery = bytes[i];
+            i += 1;
+        }
+        // temperature
+        else if (channel_id === 0x03 && channel_type === 0x67) {
+            decoded.temperature = readInt16LE(bytes.slice(i, i + 2)) / 10;
+            i += 2;
+        }
+        // temperature threshold alert
+        else if (channel_id === 0x83 && channel_type === 0x67) {
+            decoded.temperature = readInt16LE(bytes.slice(i, i + 2)) / 10;
+            decoded.temperature_alert = readAlertType(bytes[i + 2]);
+            i += 3;
+        }
+        // temperature spike alert 
+        else if (channel_id === 0x84 && channel_type === 0xd7) {
+            decoded.temperature = readInt16LE(bytes.slice(i, i + 2)) / 10;
+            decoded.temperature_change = readInt16LE(bytes.slice(i + 2, i + 4)) / 100;
+            decoded.temperature_alert = readAlertType(bytes[i + 4]);
+            i += 5;
+        }
+        // temperature history
+        else if (channel_id === 0x20 && channel_type === 0xce) {
+            if (decoded.history == undefined) {
+                decoded.history = [];
+            }
+            var item = {};
+            item.timestamp = readUInt32LE(bytes.slice(i, i + 4));
+            item.temperature = readInt16LE(bytes.slice(i + 4, i + 6)) / 10;
+            decoded.history.push(item);
+            i += 6;
+        } else {
+            break;
+        }
+    }
+
+    return decoded;
+}
+
+function readUInt16LE(bytes) {
+    var value = (bytes[1] << 8) + bytes[0];
+    return value & 0xffff;
+}
+
+function readInt16LE(bytes) {
+    var ref = readUInt16LE(bytes);
+    return ref > 0x7fff ? ref - 0x10000 : ref;
+}
+
+function readUInt32LE(bytes) {
+    var value = (bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0];
+    return value & 0xffffffff;
+}
+
+function readAlertType(type) {
+    switch (type) {
+        case 0:
+            return "normal";
+        case 1:
+            return "threshold";
+        case 2:
+            return "spike";
+        default:
+            return "unkown";
+    }
+}
