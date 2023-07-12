c693edf0f83454e878bd83d887e04518fc3d06a4
Milesight IoT <75722092+Milesight-IoT@users.noreply.github.com>
2023-07-12 09:03:56 +0800

    Update WS50X_v2_Chirpstack.js
    Change upload status open/close to on/off

--- WS_Series/WS50x/WS50X_v2_Chirpstack.js
+++ WS_Series/WS50x/WS50X_v2_Chirpstack.js
@@ -18,13 +18,13 @@ function Decode(fPort, bytes) {
             //  Switch    3 2 1   3 2 1
             //          ------- -------
             // bit mask  change   state
-            decoded.switch_1 = (bytes[i] & 1) == 1 ? "open" : "close";
+            decoded.switch_1 = (bytes[i] & 1) == 1 ? "on" : "off";
             decoded.switch_1_change = ((bytes[i] >> 4) & 1) == 1 ? "yes" : "no";
 
-            decoded.switch_2 = ((bytes[i] >> 1) & 1) == 1 ? "open" : "close";
+            decoded.switch_2 = ((bytes[i] >> 1) & 1) == 1 ? "on" : "off";
             decoded.switch_2_change = ((bytes[i] >> 5) & 1) == 1 ? "yes" : "no";
 
-            decoded.switch_3 = ((bytes[i] >> 2) & 1) == 1 ? "open" : "close";
+            decoded.switch_3 = ((bytes[i] >> 2) & 1) == 1 ? "on" : "off";
             decoded.switch_3_change = ((bytes[i] >> 6) & 1) == 1 ? "yes" : "no";
             i += 1;
         }
