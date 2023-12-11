37a570259d77d8b0c0d25601b359cac914b32cb9
Sway <sway@milesight.com>
2023-12-11 10:59:11 +0800

    fix(ws1x6): update decode

--- WS_Series/WS136&WS156/WS136_WS156_Chirpstack_v4.js
+++ WS_Series/WS136&WS156/WS136_WS156_Chirpstack_v4.js
@@ -16,6 +16,7 @@ function milesight(bytes) {
     for (var i = 0; i < bytes.length; ) {
         var channel_id = bytes[i++];
         var channel_type = bytes[i++];
+
         // BATTERY
         if (channel_id === 0x01 && channel_type === 0x75) {
             decoded.battery = bytes[i];
@@ -24,11 +25,9 @@ function milesight(bytes) {
         // PRESS STATE
         else if (channel_id === 0xff && channel_type === 0x34) {
             var id = bytes[i];
-            var command = [bytes[i + 2], bytes[i + 1]];
-            var channel_name = "button_" + id;
-            // var channel_command_name = "button_" + id + "_command";
-            decoded[channel_name] = "trigger";
-            // decoded[channel_command_name] = command;
+            var btn_chn_name = "button_" + id;
+            decoded[btn_chn_name] = "trigger";
+            decoded[btn_chn_name + "_d2d"] = readUInt16LE(bytes.slice(i + 1, i + 3));
             i += 3;
         } else {
             break;
@@ -36,4 +35,12 @@ function milesight(bytes) {
     }
 
     return decoded;
+}
+
+function readUInt16LE(bytes) {
+    return (bytes[1] << 8) | bytes[0];
+}
+
+function readD2DCommand(bytes) {
+    return ("0" + (bytes[1] & 0xff).toString(16)).slice(-2) + ("0" + (bytes[0] & 0xff).toString(16)).slice(-2);
 }
