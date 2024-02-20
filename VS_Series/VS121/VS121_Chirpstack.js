6e8322716b6c937edb1c9a40afed9a42c5451b01
Sway <sway@milesight.com>
2024-02-20 15:43:10 +0800

    vs121: add function readUInt32

--- VS_Series/VS121/VS121_Chirpstack.js
+++ VS_Series/VS121/VS121_Chirpstack.js
@@ -155,6 +155,11 @@ function readUInt16LE(bytes) {
     return value & 0xffff;
 }
 
+function readUInt32LE(bytes) {
+    var value = (bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0];
+    return (value & 0xffffffff) >>> 0;
+}
+
 function readVersion(bytes) {
     var temp = [];
     for (var idx = 0; idx < bytes.length; idx++) {
