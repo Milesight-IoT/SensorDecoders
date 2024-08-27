618e79f2fafb9b98b99109779e5843ade33b5e21
Sway <sway@milesight.com>
2024-08-27 19:46:49 +0800

    feat(ws202): encoder function

--- WS_Series/WS202/README.md
+++ WS_Series/WS202/README.md
@@ -1,18 +1,32 @@
 # PIR & Light Sensor - Milesight IoT
 
-The payload decoder function is applicable to WS202.
+The payload decoder function applies to WS202.
 
-For more detailed information, please visit [milesight official website](https://www.milesight-iot.com).
+For more detailed information, please visit Milesight's [official website](https://www.milesight-iot.com).
 
 ![WS202](WS202.png)
 
 ## Payload Definition
 
-| CHANNEL  |  ID  | TYPE | LENGTH | DESCRIPTION                                            |
-| :------: | :--: | :--: | :----: | ------------------------------------------------------ |
-| Battery  | 0x01 | 0x75 |   1    | battery(1B)<br/>battery, unit: %                       |
-|   PIR    | 0x03 | 0x00 |   1    | pir(1B)<br/>pir, values: (0: normal, 1: trigger)       |
-| Daylight | 0x04 | 0x00 |   1    | daylight(1B)<br/>daylight, values: (0: dark, 1: light) |
+### Attribute
+
+|      CHANNEL       |  ID  | TYPE | LENGTH | DESCRIPTION          |
+| :----------------: | :--: | :--: | :----: | -------------------- |
+|  Protocol Version  | 0xFF | 0x01 |   1    | protocol_version(1B) |
+|    Power Status    | 0xFF | 0x0B |   1    | power_status(1B)     |
+|   Serial Number    | 0xFF | 0x16 |   8    | sn(8B)               |
+|  Hardware Version  | 0xFF | 0x09 |   2    | hardware_version(2B) |
+|  Firmware Version  | 0xFF | 0x0A |   2    | firmware_version(2B) |
+| LoRaWAN Class Type | 0xFF | 0x0F |   1    | lorawan_class(1B)    |
+|    TSL Version     | 0xFF | 0xFF |   2    | tsl_version(2B)      |
+
+### Telemetry
+
+| CHANNEL  |  ID  | TYPE | LENGTH | DESCRIPTION                                             |
+| :------: | :--: | :--: | :----: | ------------------------------------------------------- |
+| Battery  | 0x01 | 0x75 |   1    | battery(1B)<br />battery, unit: %                       |
+|   PIR    | 0x03 | 0x00 |   1    | pir(1B)<br />pir, values: (0: normal, 1: trigger)       |
+| Daylight | 0x04 | 0x00 |   1    | daylight(1B)<br />daylight, values: (0: dark, 1: light) |
 
 ## Example
 
---
+++ WS_Series/WS202/WS202_Encoder.js
@@ -0,0 +1,183 @@
+/**
+ * Payload Encoder
+ *
+ * Copyright 2025 Milesight IoT
+ *
+ * @product WS202
+ */
+var RAW_VALUE = 0x00;
+
+// Chirpstack v4
+function encodeDownlink(input) {
+    var encoded = milesightDeviceEncode(input.data);
+    return { bytes: encoded };
+}
+
+// Chirpstack v3
+function Encode(fPort, obj) {
+    return milesightDeviceEncode(obj);
+}
+
+// The Things Network
+function Encoder(obj, port) {
+    return milesightDeviceEncode(obj);
+}
+
+function milesightDeviceEncode(payload) {
+    var encoded = [];
+
+    if ("reboot" in payload) {
+        encoded = encoded.concat(reboot(payload.reboot));
+    }
+    if ("report_interval" in payload) {
+        encoded = encoded.concat(setReportInterval(payload.report_interval));
+    }
+    if ("light_alarm_config" in payload) {
+        encoded = encoded.concat(setLightThreshold(payload.light_alarm_config));
+    }
+
+    return encoded;
+}
+
+/**
+ * reboot device
+ * @param {number} reboot values: (0: no, 1: yes)
+ * @example { "reboot": 1 }
+ */
+function reboot(reboot) {
+    var yes_no_map = { 0: "no", 1: "yes" };
+    var yes_no_values = getValues(yes_no_map);
+    if (yes_no_values.indexOf(reboot) === -1) {
+        throw new Error("reboot must be one of " + yes_no_values.join(", "));
+    }
+
+    if (getValue(yes_no_map, reboot) === 0) {
+        return [];
+    }
+    return [0xff, 0x10, 0xff];
+}
+
+/**
+ * report interval configuration
+ * @param {number} report_interval uint: second
+ * @example payload: { "report_interval": 600 }
+ */
+function setReportInterval(report_interval) {
+    if (typeof report_interval !== "number") {
+        throw new Error("report_interval must be a number");
+    }
+    if (report_interval < 1) {
+        throw new Error("report_interval must be greater than 1");
+    }
+
+    var buffer = new Buffer(4);
+    buffer.writeUInt8(0xff);
+    buffer.writeUInt8(0x03);
+    buffer.writeUInt16LE(report_interval);
+    return buffer.toBytes();
+}
+
+/**
+ * set light threshold alarm
+ * @param {object} light_alarm_config
+ * @param {number} light_alarm_config.min_threshold uint: lux
+ * @param {number} light_alarm_config.max_threshold uint: lux
+ * @example { "light_alarm_config": { "min_threshold": 300, "max_threshold": 3000 } }
+ */
+function setLightThreshold(light_alarm_config) {
+    var min_threshold = light_alarm_config.min_threshold;
+    var max_threshold = light_alarm_config.max_threshold;
+
+    if (typeof min_threshold !== "number" || typeof max_threshold !== "number") {
+        throw new Error("light_alarm_config.min_threshold and light_alarm_config.max_threshold must be numbers");
+    }
+    if (min_threshold < 0 || min_threshold > 65535 || max_threshold < 0 || max_threshold > 65535) {
+        throw new Error("light_alarm_config.min_threshold and light_alarm_config.max_threshold must be in range [0, 65535]");
+    }
+
+    var buffer = new Buffer(11);
+    buffer.writeUInt8(0xff);
+    buffer.writeUInt8(0x06);
+    buffer.writeUInt8(0x00);
+    buffer.writeUInt16LE(min_threshold);
+    buffer.writeUInt16LE(max_threshold);
+    buffer.writeUInt16LE(0x00);
+    buffer.writeUInt16LE(0x00);
+    return buffer.toBytes();
+}
+
+function getValues(map) {
+    var values = [];
+    if (RAW_VALUE) {
+        for (var key in map) {
+            values.push(parseInt(key));
+        }
+    } else {
+        for (var key in map) {
+            values.push(map[key]);
+        }
+    }
+    return values;
+}
+
+function getValue(map, value) {
+    if (RAW_VALUE) return value;
+
+    for (var key in map) {
+        if (map[key] === value) {
+            return parseInt(key);
+        }
+    }
+
+    throw new Error("not match in " + JSON.stringify(map));
+}
+function Buffer(size) {
+    this.buffer = new Array(size);
+    this.offset = 0;
+
+    for (var i = 0; i < size; i++) {
+        this.buffer[i] = 0;
+    }
+}
+
+Buffer.prototype._write = function (value, byteLength, isLittleEndian) {
+    var offset = 0;
+    for (var index = 0; index < byteLength; index++) {
+        offset = isLittleEndian ? index << 3 : (byteLength - 1 - index) << 3;
+        this.buffer[this.offset + index] = (value >> offset) & 0xff;
+    }
+};
+
+Buffer.prototype.writeUInt8 = function (value) {
+    this._write(value, 1, true);
+    this.offset += 1;
+};
+
+Buffer.prototype.writeInt8 = function (value) {
+    this._write(value < 0 ? value + 0x100 : value, 1, true);
+    this.offset += 1;
+};
+
+Buffer.prototype.writeUInt16LE = function (value) {
+    this._write(value, 2, true);
+    this.offset += 2;
+};
+
+Buffer.prototype.writeInt16LE = function (value) {
+    this._write(value < 0 ? value + 0x10000 : value, 2, true);
+    this.offset += 2;
+};
+
+Buffer.prototype.writeUInt32LE = function (value) {
+    this._write(value, 4, true);
+    this.offset += 4;
+};
+
+Buffer.prototype.writeInt32LE = function (value) {
+    this._write(value < 0 ? value + 0x100000000 : value, 4, true);
+    this.offset += 4;
+};
+
+Buffer.prototype.toBytes = function () {
+    return this.buffer;
+};
