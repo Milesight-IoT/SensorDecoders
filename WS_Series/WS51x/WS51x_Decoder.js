da566cae82bc051f58438cd4b4e04290e94e8281
Sway <sway@milesight.com>
2024-10-23 10:14:15 +0800

    feat(ws51x): temperature feature & encode function support
    - overheating alarm, overload current alarm, temperature threshold alarm
    - temperature with exception
    - support raw_value mode

--- WS_Series/WS51x/WS51x_Decoder.js
+++ WS_Series/WS51x/WS51x_Decoder.js
@@ -1,10 +1,12 @@
 /**
- * Payload Decoder
+ * Payload Encoder
  *
  * Copyright 2024 Milesight IoT
  *
- * @product WS513 / WS515
+ * @product WS51x
  */
+var RAW_VALUE = 0x01;
+
 // Chirpstack v4
 function decodeUplink(input) {
     var decoded = milesightDeviceDecode(input.bytes);
@@ -28,24 +30,64 @@ function milesightDeviceDecode(bytes) {
         var channel_id = bytes[i++];
         var channel_type = bytes[i++];
 
+        // IPSO VERSION
+        if (channel_id === 0xff && channel_type === 0x01) {
+            decoded.ipso_version = readProtocolVersion(bytes[i]);
+            i += 1;
+        }
+        // HARDWARE VERSION
+        else if (channel_id === 0xff && channel_type === 0x09) {
+            decoded.hardware_version = readHardwareVersion(bytes.slice(i, i + 2));
+            i += 2;
+        }
+        // FIRMWARE VERSION
+        else if (channel_id === 0xff && channel_type === 0x0a) {
+            decoded.firmware_version = readFirmwareVersion(bytes.slice(i, i + 2));
+            i += 2;
+        }
+        // TSL VERSION
+        else if (channel_id === 0xff && channel_type === 0xff) {
+            decoded.tsl_version = readTslVersion(bytes.slice(i, i + 2));
+            i += 2;
+        }
+        // SERIAL NUMBER
+        else if (channel_id === 0xff && channel_type === 0x16) {
+            decoded.sn = readSerialNumber(bytes.slice(i, i + 8));
+            i += 8;
+        }
+        // LORAWAN CLASS TYPE
+        else if (channel_id === 0xff && channel_type === 0x0f) {
+            decoded.lorawan_class = readLoRaWANClass(bytes[i]);
+            i += 1;
+        }
+        // RESET EVENT
+        else if (channel_id === 0xff && channel_type === 0xfe) {
+            decoded.reset_event = readResetEvent(1);
+            i += 1;
+        }
+        // DEVICE STATUS
+        else if (channel_id === 0xff && channel_type === 0x0b) {
+            decoded.device_status = readDeviceStatus(1);
+            i += 1;
+        }
         // VOLTAGE
-        if (channel_id === 0x03 && channel_type === 0x74) {
+        else if (channel_id === 0x03 && channel_type === 0x74) {
             decoded.voltage = readUInt16LE(bytes.slice(i, i + 2)) / 10;
             i += 2;
         }
         // ACTIVE POWER
         else if (channel_id === 0x04 && channel_type === 0x80) {
-            decoded.power = readUInt32LE(bytes.slice(i, i + 4));
+            decoded.active_power = readUInt32LE(bytes.slice(i, i + 4));
             i += 4;
         }
         // POWER FACTOR
         else if (channel_id === 0x05 && channel_type === 0x81) {
-            decoded.factor = bytes[i];
+            decoded.power_factor = readUInt8(bytes[i]);
             i += 1;
         }
         // POWER CONSUMPTION
         else if (channel_id === 0x06 && channel_type == 0x83) {
-            decoded.power_sum = readUInt32LE(bytes.slice(i, i + 4));
+            decoded.power_consumption = readUInt32LE(bytes.slice(i, i + 4));
             i += 4;
         }
         // CURRENT
@@ -55,8 +97,33 @@ function milesightDeviceDecode(bytes) {
         }
         // STATE
         else if (channel_id === 0x08 && channel_type == 0x70) {
-            decoded.state = bytes[i] === 1 ? "open" : "close";
+            decoded.socket_status = readSocketStatus(bytes[i]);
             i += 1;
+        }
+        // TEMPERATURE (@since v1.9)
+        else if (channel_id === 0x09 && channel_type === 0x67) {
+            var temperature_value = readUInt16LE(bytes.slice(i, i + 2));
+            if (temperature_value === 0xfffd) {
+                decoded.temperature_sensor_status = readSensorStatus(1);
+            } else if (temperature_value === 0xffff) {
+                decoded.temperature_sensor_status = readSensorStatus(2);
+            } else {
+                decoded.temperature = readInt16LE(bytes.slice(i, i + 2)) / 10;
+            }
+            i += 2;
+        }
+        // TEMPERATURE THRESHOLD ALARM (@since v2.1)
+        else if (channel_id === 0x89 && channel_type === 0x67) {
+            decoded.temperature = readInt16LE(bytes.slice(i, i + 2)) / 10;
+            decoded.temperature_alarm = readTemperatureAlarm(bytes[i + 2]);
+            i += 3;
+        }
+        // TEMPERATURE MUTATION ALARM (@since v1.9 @deprecated v2.1)
+        else if (channel_id === 0x99 && channel_type === 0x67) {
+            decoded.temperature = readInt16LE(bytes.slice(i, i + 2)) / 10;
+            decoded.temperature_mutation = readInt16LE(bytes.slice(i + 2, i + 4)) / 10;
+            decoded.temperature_alarm = readTemperatureAlarm(3);
+            i += 5;
         } else {
             break;
         }
@@ -64,9 +131,25 @@ function milesightDeviceDecode(bytes) {
     return decoded;
 }
 
-/* ******************************************
- * bytes to number
- ********************************************/
+function readUInt8(bytes) {
+    return bytes & 0xff;
+}
+
+function readInt8(bytes) {
+    var ref = readUInt8(bytes);
+    return ref > 0x7f ? ref - 0x100 : ref;
+}
+
+function readUInt16LE(bytes) {
+    var value = (bytes[1] << 8) + bytes[0];
+    return value & 0xffff;
+}
+
+function readInt16LE(bytes) {
+    var ref = readUInt16LE(bytes);
+    return ref > 0x7fff ? ref - 0x10000 : ref;
+}
+
 function readUInt16LE(bytes) {
     var value = (bytes[1] << 8) + bytes[0];
     return value & 0xffff;
@@ -75,4 +158,97 @@ function readUInt16LE(bytes) {
 function readUInt32LE(bytes) {
     var value = (bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0];
     return (value & 0xffffffff) >>> 0;
+}
+
+function getValue(map, key) {
+    if (RAW_VALUE) return key;
+
+    var value = map[key];
+    if (!value) value = "unknown";
+    return value;
+}
+
+function readProtocolVersion(bytes) {
+    var major = (bytes & 0xf0) >> 4;
+    var minor = bytes & 0x0f;
+    return "v" + major + "." + minor;
+}
+
+function readHardwareVersion(bytes) {
+    var major = bytes[0] & 0xff;
+    var minor = (bytes[1] & 0xff) >> 4;
+    return "v" + major + "." + minor;
+}
+
+function readFirmwareVersion(bytes) {
+    var major = bytes[0] & 0xff;
+    var minor = bytes[1] & 0xff;
+    return "v" + major + "." + minor;
+}
+
+function readTslVersion(bytes) {
+    var major = bytes[0] & 0xff;
+    var minor = bytes[1] & 0xff;
+    return "v" + major + "." + minor;
+}
+
+function readSerialNumber(bytes) {
+    var temp = [];
+    for (var idx = 0; idx < bytes.length; idx++) {
+        temp.push(("0" + (bytes[idx] & 0xff).toString(16)).slice(-2));
+    }
+    return temp.join("");
+}
+
+function readLoRaWANClass(type) {
+    var class_map = {
+        0: "Class A",
+        1: "Class B",
+        2: "Class C",
+        3: "Class CtoB",
+    };
+    return getValue(class_map, type);
+}
+
+function readResetEvent(status) {
+    var status_map = {
+        0: "normal",
+        1: "reset",
+    };
+    return getValue(status_map, status);
+}
+
+function readDeviceStatus(status) {
+    var status_map = {
+        0: "off",
+        1: "on",
+    };
+    return getValue(status_map, status);
+}
+
+function readSocketStatus(status) {
+    var status_map = {
+        0: "off",
+        1: "on",
+    };
+    return getValue(status_map, status);
+}
+
+function readSensorStatus(type) {
+    var status_map = {
+        0: "normal",
+        1: "over range alarm",
+        2: "read failed",
+    };
+    return getValue(status_map, type);
+}
+
+function readTemperatureAlarm(type) {
+    var status_map = {
+        0: "threshold alarm release",
+        1: "threshold alarm",
+        2: "overheat alarm",
+        3: "mutation alarm",
+    };
+    return getValue(status_map, type);
 }
