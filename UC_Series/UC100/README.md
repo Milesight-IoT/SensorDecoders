b19681a47f8706224baae47908be3034d919d88e
Sway <sway@milesight.com>
2024-12-18 17:31:28 +0800

    feat(uc100): modbus alarm, modbus history, text message

--- UC_Series/UC100/README.md
+++ UC_Series/UC100/README.md
@@ -8,12 +8,57 @@ For more detailed information, please visit [milesight official website](https:/
 
 ## Payload Definition
 
-|     CHANNEL      |  ID  | TYPE | LENGTH | DESCRIPTION                                                           |
-| :--------------: | :--: | :--: | :----: | --------------------------------------------------------------------- |
-| Protocol Version | 0xFF | 0x01 |   1    | ipso_version(1B)                                                      |
-|   Power Status   | 0xFF | 0x0B |   1    | power(1B)                                                             |
-|  Serial Number   | 0xFF | 0x16 |   8    | sn(8B)                                                                |
-| Hardware Version | 0xFF | 0x09 |   2    | hardware_version(2B)                                                  |
-| Firmware Version | 0xFF | 0x0A |   2    | firmware_version(2B)                                                  |
-|      MODBUS      | 0xFF | 0x19 |   N    | modbus_chn_id(1B) + data_size(1B) + data_type(1B) + channel_value(MB) |
-|   MODBUS ERROR   | 0xFF | 0x15 |   1    | modbus_chn_id_alarm(1B)                                               |
+|     CHANNEL      |  ID  | TYPE | LENGTH | DESCRIPTION                                                                                                                                                                                          |
+| :--------------: | :--: | :--: | :----: | ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+|   IPSO Version   | 0xFF | 0x01 |   1    | ipso_version(1B)                                                                                                                                                                                     |
+|  Device Status   | 0xFF | 0x0B |   1    | device_status(1B)                                                                                                                                                                                    |
+|  Serial Number   | 0xFF | 0x16 |   8    | sn(8B)                                                                                                                                                                                               |
+| Hardware Version | 0xFF | 0x09 |   2    | hardware_version(2B)                                                                                                                                                                                 |
+| Firmware Version | 0xFF | 0x0A |   2    | firmware_version(2B)                                                                                                                                                                                 |
+|      Modbus      | 0xFF | 0x19 |   N    | modbus_chn_id(1B) + data_size(1B) + data_type(1B) + channel_value(MB)                                                                                                                                |
+|   Modbus Error   | 0xFF | 0x15 |   1    | modbus_chn_id(1B)                                                                                                                                                                                    |
+|   Modbus Alarm   | 0xFF | 0xEE |   N    | modbus_chn_def(1B) + data_length(1B) + modbus_chn_data_def(1B) + data(MB)<br/>modbus_chn_def, modbus_chn_id(0..5) + modbus_chn_event(6..7)<br/>modbus_chn_data_def, modbus_data_type(0..6) + sign(7) |
+|  Modbus History  | 0x20 | 0xCE |   10   | timestamp(4B) + data_length(1B) + modbus_chn_data_def(1B) + data(4B)<br/>modbus_chn_data_def, reserved(1) + modbus_data_type(2..6) + sign(7)                                                         |
+| Message Histroy  | 0x20 | 0xCD |   N    | timestamp(4B) + size(1B) + data(MB)                                                                                                                                                                  |
+
+### MODBUS DATA DEFINITION
+
+**modbus_chn_event**
+
+| ID  | EVENT TYPE              |
+| :-: | :---------------------- |
+|  0  | normal                  |
+|  1  | threshold alarm         |
+|  2  | threshold release alarm |
+|  3  | mutation alarm          |
+
+**modbus_data_type**
+
+| ID  | DATA TYPE               |
+| :-: | :---------------------- |
+|  0  | MB_REG_COIL             |
+|  1  | MB_REG_DIS              |
+|  2  | MB_REG_INPUT_AB         |
+|  3  | MB_REG_INPUT_BA         |
+|  4  | MB_REG_INPUT_INT32_ABCD |
+|  5  | MB_REG_INPUT_INT32_BADC |
+|  6  | MB_REG_INPUT_INT32_CDAB |
+|  7  | MB_REG_INPUT_INT32_DCBA |
+|  8  | MB_REG_INPUT_INT32_AB   |
+|  9  | MB_REG_INPUT_INT32_CD   |
+| 10  | MB_REG_INPUT_FLOAT_ABCD |
+| 11  | MB_REG_INPUT_FLOAT_BADC |
+| 12  | MB_REG_INPUT_FLOAT_CDAB |
+| 13  | MB_REG_INPUT_FLOAT_DCBA |
+| 14  | MB_REG_HOLD_INT16_AB    |
+| 15  | MB_REG_HOLD_INT16_BA    |
+| 16  | MB_REG_HOLD_INT32_ABCD  |
+| 17  | MB_REG_HOLD_INT32_BADC  |
+| 18  | MB_REG_HOLD_INT32_CDAB  |
+| 19  | MB_REG_HOLD_INT32_DCBA  |
+| 20  | MB_REG_HOLD_INT32_AB    |
+| 21  | MB_REG_HOLD_INT32_CD    |
+| 22  | MB_REG_HOLD_FLOAT_ABCD  |
+| 23  | MB_REG_HOLD_FLOAT_BADC  |
+| 24  | MB_REG_HOLD_FLOAT_CDAB  |
+| 25  | MB_REG_HOLD_FLOAT_DCBA  |
--- UC_Series/UC100/UC100_Decoder.js
+++ UC_Series/UC100/UC100_Decoder.js
@@ -27,20 +27,10 @@ function milesightDeviceDecode(bytes) {
         var channel_id = bytes[i++];
         var channel_type = bytes[i++];
 
-        // POWER
-        if (channel_id === 0xff && channel_type === 0x0b) {
-            decoded.power = "on";
-            i += 1;
-        }
         // IPSO VERSION
-        else if (channel_id === 0xff && channel_type === 0x01) {
-            decoded.protocol_version = readProtocolVersion(bytes[i]);
+        if (channel_id === 0xff && channel_type === 0x01) {
+            decoded.ipso_version = readProtocolVersion(bytes[i]);
             i += 1;
-        }
-        // SERIAL NUMBER
-        else if (channel_id === 0xff && channel_type === 0x16) {
-            decoded.sn = readSerialNumber(bytes.slice(i, i + 8));
-            i += 8;
         }
         // HARDWARE VERSION
         else if (channel_id === 0xff && channel_type === 0x09) {
@@ -52,21 +42,28 @@ function milesightDeviceDecode(bytes) {
             decoded.firmware_version = readFirmwareVersion(bytes.slice(i, i + 2));
             i += 2;
         }
+        // DEVICE STATUS
+        else if (channel_id === 0xff && channel_type === 0x0b) {
+            decoded.device_status = "on";
+            i += 1;
+        }
+        // SERIAL NUMBER
+        else if (channel_id === 0xff && channel_type === 0x16) {
+            decoded.sn = readSerialNumber(bytes.slice(i, i + 8));
+            i += 8;
+        }
         // MODBUS
         else if (channel_id === 0xff && channel_type === 0x19) {
             var modbus_chn_id = bytes[i++] + 1;
             var data_length = bytes[i++];
-            var data_type = bytes[i++];
-            var sign = (data_type >>> 7) & 0x01;
-            var type = data_type & 0x7f; // 0b01111111
+            var data_def = bytes[i++];
+            var sign = (data_def >>> 7) & 0x01;
+            var data_type = data_def & 0x7f; // 0b01111111
             var modbus_chn_name = "modbus_chn_" + modbus_chn_id;
-            switch (type) {
+            switch (data_type) {
                 case 0:
-                    decoded[chn] = bytes[i] ? "on" : "off";
-                    i += 1;
-                    break;
                 case 1:
-                    decoded[chn] = bytes[i];
+                    decoded[modbus_chn_name] = bytes[i] ? "on" : "off";
                     i += 1;
                     break;
                 case 2:
@@ -96,8 +93,138 @@ function milesightDeviceDecode(bytes) {
             var channel_name = "modbus_chn_" + modbus_chn_id + "_alarm";
             decoded[channel_name] = "read error";
             i += 1;
-        } else {
-            break;
+        }
+        // MODBUS ALARM (v1.7+)
+        else if (channel_id === 0xff && channel_type === 0xee) {
+            var chn_def = bytes[i++];
+            var data_length = bytes[i++];
+            var data_def = bytes[i++];
+
+            var modbus_chn_id = (chn_def & 0x1f) + 1;
+            var modbus_alarm = readModbusAlarmType(chn_def >>> 6);
+            var sign = (data_def >>> 7) & 0x01;
+            var data_type = data_def & 0x7f;
+
+            var modbus_chn_name = "modbus_chn_" + modbus_chn_id;
+            decoded[modbus_chn_name + "_alarm"] = modbus_alarm;
+            switch (data_type) {
+                case 0: // MB_REG_COIL
+                case 1: // MB_REG_DISCRETE
+                    decoded[modbus_chn_name] = bytes[i] ? "on" : "off";
+                    i += 1;
+                    break;
+                case 2: // MB_REG_INPUT_AB
+                case 3: // MB_REG_INPUT_BA
+                case 14: // MB_REG_HOLD_INT16_AB
+                case 15: // MB_REG_HOLD_INT16_BA
+                    decoded[modbus_chn_name] = sign ? readInt16LE(bytes.slice(i, i + 2)) : readUInt16LE(bytes.slice(i, i + 2));
+                    i += 2;
+                    break;
+                case 4: // MB_REG_INPUT_INT32_ABCD
+                case 5: // MB_REG_INPUT_INT32_BADC
+                case 6: // MB_REG_INPUT_INT32_CDAB
+                case 7: // MB_REG_INPUT_INT32_DCBA
+                case 16: // MB_REG_HOLD_INT32_ABCD
+                case 17: // MB_REG_HOLD_INT32_BADC
+                case 18: // MB_REG_HOLD_INT32_CDAB
+                case 19: // MB_REG_HOLD_INT32_DCBA
+                case 8: // MB_REG_INPUT_INT32_AB
+                case 9: // MB_REG_INPUT_INT32_CD
+                case 20: // MB_REG_HOLD_INT32_AB
+                case 21: // MB_REG_HOLD_INT32_CD
+                    decoded[modbus_chn_name] = sign ? readInt32LE(bytes.slice(i, i + 4)) : readUInt32LE(bytes.slice(i, i + 4));
+                    i += 4;
+                    break;
+                case 10: // MB_REG_INPUT_FLOAT_ABCD
+                case 11: // MB_REG_INPUT_FLOAT_BADC
+                case 12: // MB_REG_INPUT_FLOAT_CDAB
+                case 13: // MB_REG_INPUT_FLOAT_DCBA
+                case 22: // MB_REG_HOLD_FLOAT_ABCD
+                case 23: // MB_REG_HOLD_FLOAT_BADC
+                case 24: // MB_REG_HOLD_FLOAT_CDAB
+                case 25: // MB_REG_HOLD_FLOAT_DCBA
+                    decoded[modbus_chn_name] = readFloatLE(bytes.slice(i, i + 4));
+                    i += 4;
+                    break;
+            }
+        }
+        // MODBUS HISTORY (v1.7+)
+        else if (channel_id === 0x20 && channel_type === 0xce) {
+            var timestamp = readUInt32LE(bytes.slice(i, i + 4));
+            var chn_id = bytes[i + 4] + 1;
+            var data_def = bytes[i + 5];
+            var sign = (data_def >>> 7) & 0x01;
+            var data_type = (data_def >> 2) & 0x1f;
+            var read_status = (data_def >>> 1) & 0x01;
+            i += 6;
+
+            var data = {};
+            data.timestamp = timestamp;
+            var modbus_chn_name = "modbus_chn_" + chn_id;
+            // READ FAILED
+            if (read_status === 0) {
+                data[modbus_chn_name + "_alarm"] = "read error";
+            } else {
+                switch (data_type) {
+                    case 0: // MB_REG_COIL
+                    case 1: // MB_REG_DISCRETE
+                        data[modbus_chn_name] = bytes[i] ? "on" : "off";
+                        i += 4;
+                        break;
+                    case 2: // MB_REG_INPUT_AB
+                    case 3: // MB_REG_INPUT_BA
+                    case 14: // MB_REG_HOLD_INT16_AB
+                    case 15: // MB_REG_HOLD_INT16_BA
+                    case 4: // MB_REG_INPUT_INT32_ABCD
+                    case 5: // MB_REG_INPUT_INT32_BADC
+                    case 6: // MB_REG_INPUT_INT32_CDAB
+                    case 7: // MB_REG_INPUT_INT32_DCBA
+                    case 16: // MB_REG_HOLD_INT32_ABCD
+                    case 17: // MB_REG_HOLD_INT32_BADC
+                    case 18: // MB_REG_HOLD_INT32_CDAB
+                    case 19: // MB_REG_HOLD_INT32_DCBA
+                    case 8: // MB_REG_INPUT_INT32_AB
+                    case 9: // MB_REG_INPUT_INT32_CD
+                    case 20: // MB_REG_HOLD_INT32_AB
+                    case 21: // MB_REG_HOLD_INT32_CD
+                        data[modbus_chn_name] = sign ? readInt32LE(bytes.slice(i, i + 4)) : readUInt32LE(bytes.slice(i, i + 4));
+                        i += 4;
+                        break;
+                    case 10: // MB_REG_INPUT_FLOAT_ABCD
+                    case 11: // MB_REG_INPUT_FLOAT_BADC
+                    case 12: // MB_REG_INPUT_FLOAT_CDAB
+                    case 13: // MB_REG_INPUT_FLOAT_DCBA
+                    case 22: // MB_REG_HOLD_FLOAT_ABCD
+                    case 23: // MB_REG_HOLD_FLOAT_BADC
+                    case 24: // MB_REG_HOLD_FLOAT_CDAB
+                    case 25: // MB_REG_HOLD_FLOAT_DCBA
+                        data[modbus_chn_name] = readFloatLE(bytes.slice(i, i + 4));
+                        i += 4;
+                        break;
+                }
+            }
+
+            decoded.history = decoded.history || [];
+            decoded.history.push(data);
+        }
+        // CUSTOM MESSAGE HISTORY (v1.7+)
+        else if (channel_id === 0x20 && channel_type === 0xcd) {
+            var timestamp = readUInt32LE(bytes.slice(i, i + 4));
+            var msg_length = bytes[i + 4];
+            var msg = readAscii(bytes.slice(i + 5, i + 5 + msg_length));
+            i += 5 + msg_length;
+
+            var data = {};
+            data.timestamp = timestamp;
+            data.custom_message = msg;
+
+            decoded.history = decoded.history || [];
+            decoded.history.push(data);
+        }
+        // CUSTOM MESSAGE
+        else {
+            decoded.custom_message = readAscii(bytes.slice(i - 2, bytes.length));
+            i = bytes.length;
         }
     }
     return decoded;
@@ -170,4 +297,27 @@ function readSerialNumber(bytes) {
         temp.push(("0" + (bytes[idx] & 0xff).toString(16)).slice(-2));
     }
     return temp.join("");
+}
+
+function readAscii(bytes) {
+    var str = "";
+    for (var i = 0; i < bytes.length; i++) {
+        str += String.fromCharCode(bytes[i]);
+    }
+    return str;
+}
+
+function readModbusAlarmType(type) {
+    switch (type) {
+        case 0x00:
+            return "Normal";
+        case 0x01:
+            return "Threshold Alarm";
+        case 0x02:
+            return "Threshold Release Alarm";
+        case 0x03:
+            return "Mutation Alarm";
+        default:
+            return "Unknown";
+    }
 }
