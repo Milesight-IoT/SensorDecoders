b543170b6ca09a72b75c44149aedec24e91429e6
Sway <sway@milesight.com>
2024-12-26 09:01:03 +0800

    feat(ws558): Smart Light Controller

---
+++ WS_Series/WS558/README.md
@@ -0,0 +1,50 @@
+# Smart Light Controller - Milesight IoT
+
+The payload decoder function is applicable to WS558.
+
+For more detailed information, please visit [milesight official website](https://www.milesight-iot.com).
+
+|        WS558        |        WS558-LN         |
+| :-----------------: | :---------------------: |
+| ![WS558](WS558.png) | ![WS558LN](WS558LN.png) |
+
+## Payload Definition
+
+|      CHANNEL      |  ID  | TYPE | LENGTH | DESCRIPTION                                           |
+| :---------------: | :--: | :--: | :----: | ----------------------------------------------------- |
+|      Voltage      | 0x03 | 0x74 |   2    | voltage(2B)<br/>voltage, read: uint16/10              |
+|   Active power    | 0x04 | 0x80 |   4    | power(4B)<br/>power, read: uint32, unit: W            |
+|   Active factor   | 0x05 | 0x81 |   1    | factor(1B)<br/>factor, read: uint8, unit: %           |
+| Power Consumption | 0x06 | 0x83 |   4    | power_sum(4B)<br/>power_sum, read: uint32, unit: W\*h |
+|      Current      | 0x07 | 0xC9 |   2    | current(2B)<br/>current, read: uint16, unit: mA       |
+|      Switch       | 0x08 | 0x31 |   2    |                                                       |
+
+### Switch Value Definition
+
+```
+binary:  0 0 0 0 0 0 0 0  0 0 0 0 0 0 0 0
+switch:  8 7 6 5 4 3 2 1  8 7 6 5 4 3 2 1
+         ---------------  ---------------
+bitmask:          change            state
+```
+
+## Example
+
+```json
+// 08310001 058164 07C90200 0374B208 068301000000 048001000000
+{
+    "voltage": 222.6,
+    "active_power": 1,
+    "power_factor": 100,
+    "total_current": 2,
+    "power_consumption": 1,
+    "switch_1": "on",
+    "switch_2": "off",
+    "switch_3": "off",
+    "switch_4": "off",
+    "switch_5": "off",
+    "switch_6": "off",
+    "switch_7": "off",
+    "switch_8": "off"
+}
+```
---
+++ WS_Series/WS558/WS558_Decoder.js
@@ -0,0 +1,104 @@
+/**
+ * Payload Decoder
+ *
+ * Copyright 2024 Milesight IoT
+ *
+ * @product WS558
+ */
+// Chirpstack v4
+function decodeUplink(input) {
+    var decoded = milesightDeviceDecode(input.bytes);
+    return { data: decoded };
+}
+
+// Chirpstack v3
+function Decode(fPort, bytes) {
+    return milesightDeviceDecode(bytes);
+}
+
+// The Things Network
+function Decoder(bytes, port) {
+    return milesightDeviceDecode(bytes);
+}
+
+function milesightDeviceDecode(bytes) {
+    var decoded = {};
+
+    for (var i = 0; i < bytes.length; ) {
+        var channel_id = bytes[i++];
+        var channel_type = bytes[i++];
+        // VOLTAGE
+        if (channel_id === 0x03 && channel_type === 0x74) {
+            decoded.voltage = readUInt16LE(bytes.slice(i, i + 2)) / 10;
+            i += 2;
+        }
+        // ACTIVE POWER
+        else if (channel_id === 0x04 && channel_type === 0x80) {
+            decoded.active_power = readUInt32LE(bytes.slice(i, i + 4));
+            i += 4;
+        }
+        // POWER FACTOR
+        else if (channel_id === 0x05 && channel_type === 0x81) {
+            decoded.power_factor = bytes[i];
+            i += 1;
+        }
+        // POWER CONSUMPTION
+        else if (channel_id === 0x06 && channel_type === 0x83) {
+            decoded.power_consumption = readUInt32LE(bytes.slice(i, i + 4));
+            i += 4;
+        }
+        // TOTAL CURRENT
+        else if (channel_id === 0x07 && channel_type === 0xc9) {
+            decoded.total_current = readUInt16LE(bytes.slice(i, i + 2));
+            i += 2;
+        }
+        // SWITCH STATUS
+        else if (channel_id === 0x08 && channel_type === 0x31) {
+            var switchFlags = bytes[i + 1];
+
+            // output all switch status
+            for (var idx = 0; idx < 8; idx++) {
+                var switchTag = "switch_" + (idx + 1);
+                decoded[switchTag] = (switchFlags >> idx) & (1 === 1) ? "on" : "off";
+            }
+
+            i += 2;
+        } else {
+            break;
+        }
+    }
+
+    return decoded;
+}
+
+/* ******************************************
+ * bytes to number
+ ********************************************/
+function readUInt8(bytes) {
+    return bytes & 0xff;
+}
+
+function readInt8(bytes) {
+    var ref = readUInt8(bytes);
+    return ref > 0x7f ? ref - 0x100 : ref;
+}
+
+function readUInt16LE(bytes) {
+    var value = (bytes[1] << 8) + bytes[0];
+    return value & 0xffff;
+}
+
+function readInt16LE(bytes) {
+    var ref = readUInt16LE(bytes);
+    return ref > 0x7fff ? ref - 0x10000 : ref;
+}
+
+function readUInt32LE(bytes) {
+    var value = (bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0];
+    return value & 0xffffffff;
+}
+
+function readInt32LE(bytes) {
+    var ref = readUInt32LE(bytes);
+    return ref > 0x7fffffff ? ref - 0x100000000 : ref;
+}
