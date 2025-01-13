dfe638151fdacc1703a2ace042c2e027d65b747b
Sway <sway@milesight.com>
2025-01-13 16:19:16 +0800

    feat(ds3604): text encoder function

--- DS_Series/DS3604/DS3604_Decoder.js
+++ DS_Series/DS3604/DS3604_Decoder.js
@@ -1,7 +1,7 @@
 /**
  * Payload Decoder
  *
- * Copyright 2024 Milesight IoT
+ * Copyright 2025 Milesight IoT
  *
  * @product DS3604
  */
@@ -46,12 +46,12 @@ function milesightDeviceDecode(bytes) {
             if (block_id < 10) {
                 block_name = "text_" + (block_id + 1);
                 block_length = bytes[i++];
-                decoded[block_name] = fromUtf8Bytes(bytes.slice(i, i + block_length));
+                decoded[block_name] = decodeUtf8(bytes.slice(i, i + block_length));
                 i += block_length;
             } else if (block_id == 10) {
-                block_name = "qrcode";
+                block_name = "qrCode";
                 block_length = bytes[i++];
-                decoded[block_name] = fromUtf8Bytes(bytes.slice(i, i + block_length));
+                decoded[block_name] = decodeUtf8(bytes.slice(i, i + block_length));
                 i += block_length;
             }
             decoded.template = template_id;
@@ -63,12 +63,30 @@ function milesightDeviceDecode(bytes) {
     return decoded;
 }
 
-function fromUtf8Bytes(bytes) {
-    return decodeURIComponent(
-        bytes
-            .map(function (ch) {
-                return "%" + (ch < 16 ? "0" : "") + ch.toString(16);
-            })
-            .join("")
-    );
+function decodeUtf8(bytes) {
+    var str = "";
+    var i = 0;
+    var byte1, byte2, byte3, byte4;
+    while (i < bytes.length) {
+        byte1 = bytes[i++];
+        if (byte1 <= 0x7f) {
+            str += String.fromCharCode(byte1);
+        } else if (byte1 <= 0xdf) {
+            byte2 = bytes[i++];
+            str += String.fromCharCode(((byte1 & 0x1f) << 6) | (byte2 & 0x3f));
+        } else if (byte1 <= 0xef) {
+            byte2 = bytes[i++];
+            byte3 = bytes[i++];
+            str += String.fromCharCode(((byte1 & 0x0f) << 12) | ((byte2 & 0x3f) << 6) | (byte3 & 0x3f));
+        } else if (byte1 <= 0xf7) {
+            byte2 = bytes[i++];
+            byte3 = bytes[i++];
+            byte4 = bytes[i++];
+            codepoint = ((byte1 & 0x07) << 18) | ((byte2 & 0x3f) << 12) | ((byte3 & 0x3f) << 6) | (byte4 & 0x3f);
+            codepoint -= 0x10000;
+            str += String.fromCharCode((codepoint >> 10) + 0xd800);
+            str += String.fromCharCode((codepoint & 0x3ff) + 0xdc00);
+        }
+    }
+    return str;
 }
---
+++ DS_Series/DS3604/DS3604_Encoder.js
@@ -0,0 +1,174 @@
+/**
+ * Payload Encoder
+ *
+ * Copyright 2025 Milesight IoT
+ *
+ * @product DS3604
+ */
+var RAW_VALUE = 0x01;
+
+// Chirpstack v4
+function encodeDownlink(input) {
+    var encoded = milesightDeviceEncode(input.data);
+    return { bytes: encoded };
+}
+
+// Chirpstack v3
+function Encode(fPort, obj) {
+    return milesightDeviceEncode(obj);
+}
+
+// The Things Network
+function Encoder(obj, port) {
+    return milesightDeviceEncode(obj);
+}
+
+function milesightDeviceEncode(payload) {
+    var encoded = [];
+
+    if ("template_id" in payload) {
+        encoded = encoded.concat(setText(payload.template_id, payload));
+    }
+
+    return encoded;
+}
+
+function setText(template_id, contents) {
+    var template_values = [1, 2];
+    var content_id_map = { text_1: 0, text_2: 1, text_3: 2, text_4: 3, text_5: 4, text_6: 5, text_7: 6, text_8: 7, text_9: 8, text_10: 9, qrCode: 10 };
+
+    if (template_values.indexOf(template_id) === -1) {
+        throw new Error("template_id must be one of " + template_values.join(", "));
+    }
+
+    var encoded = [];
+    for (var key in content_id_map) {
+        if (key in contents) {
+            encoded = encoded.concat(encodeContent(template_id - 1, content_id_map[key], contents[key]));
+        }
+    }
+
+    return encoded;
+}
+
+function encodeContent(template_id, content_id, content) {
+    var bytes = encodeUtf8(content);
+
+    var data = 0x00;
+    data |= template_id << 6;
+    data |= content_id << 0;
+
+    var buffer = new Buffer(4 + bytes.length);
+    buffer.writeUInt8(0xfb);
+    buffer.writeUInt8(0x01); // content
+    buffer.writeUInt8(data);
+    buffer.writeUInt8(bytes.length);
+    buffer.writeBytes(bytes);
+    return buffer.toBytes();
+}
+
+function encodeUtf8(str) {
+    var byteArray = [];
+    for (var i = 0; i < str.length; i++) {
+        var charCode = str.charCodeAt(i);
+        if (charCode < 0x80) {
+            byteArray.push(charCode);
+        } else if (charCode < 0x800) {
+            byteArray.push(0xc0 | (charCode >> 6));
+            byteArray.push(0x80 | (charCode & 0x3f));
+        } else if (charCode < 0x10000) {
+            byteArray.push(0xe0 | (charCode >> 12));
+            byteArray.push(0x80 | ((charCode >> 6) & 0x3f));
+            byteArray.push(0x80 | (charCode & 0x3f));
+        } else if (charCode < 0x200000) {
+            byteArray.push(0xf0 | (charCode >> 18));
+            byteArray.push(0x80 | ((charCode >> 12) & 0x3f));
+            byteArray.push(0x80 | ((charCode >> 6) & 0x3f));
+            byteArray.push(0x80 | (charCode & 0x3f));
+        }
+    }
+    return byteArray;
+}
+
+function getValues(map) {
+    var values = [];
+    if (RAW_VALUE) {
+        for (var key in map) {
+            values.push(parseInt(key));
+        }
+    } else {
+        for (var key in map) {
+            values.push(map[key]);
+        }
+    }
+    return values;
+}
+
+function getValue(map, value) {
+    if (RAW_VALUE) return value;
+
+    for (var key in map) {
+        if (map[key] === value) {
+            return key;
+        }
+    }
+
+    throw new Error("not match in " + JSON.stringify(map));
+}
+
+function Buffer(size) {
+    this.buffer = new Array(size);
+    this.offset = 0;
+
+    for (var i = 0; i < size; i++) {
+        this.buffer[i] = 0;
+    }
+}
+
+Buffer.prototype._write = function (value, byteLength, isLittleEndian) {
+    for (var index = 0; index < byteLength; index++) {
+        var shift = isLittleEndian ? index << 3 : (byteLength - 1 - index) << 3;
+        this.buffer[this.offset + index] = (value & (0xff << shift)) >> shift;
+    }
+};
+
+Buffer.prototype.writeUInt8 = function (value) {
+    this._write(value, 1, true);
+    this.offset += 1;
+};
+
+Buffer.prototype.writeInt8 = function (value) {
+    this._write(value < 0 ? value + 0x100 : value, 1, true);
+    this.offset += 1;
+};
+
+Buffer.prototype.writeUInt16LE = function (value) {
+    this._write(value, 2, true);
+    this.offset += 2;
+};
+
+Buffer.prototype.writeInt16LE = function (value) {
+    this._write(value < 0 ? value + 0x10000 : value, 2, true);
+    this.offset += 2;
+};
+
+Buffer.prototype.writeUInt32LE = function (value) {
+    this._write(value, 4, true);
+    this.offset += 4;
+};
+
+Buffer.prototype.writeInt32LE = function (value) {
+    this._write(value < 0 ? value + 0x100000000 : value, 4, true);
+    this.offset += 4;
+};
+
+Buffer.prototype.writeBytes = function (bytes) {
+    for (var i = 0; i < bytes.length; i++) {
+        this.buffer[this.offset + i] = bytes[i];
+    }
+    this.offset += bytes.length;
+};
+
+Buffer.prototype.toBytes = function () {
+    return this.buffer;
+};
