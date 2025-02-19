ba01ccb5d88984b933e8d33388eb78ef102bdf1d
Sway <sway@milesight.com>
2025-02-19 17:51:45 +0800

    fix(uc521): ff55 data

--- UC_Series/UC521/UC521_Decoder.js
+++ UC_Series/UC521/UC521_Decoder.js
@@ -205,7 +205,7 @@ function milesightDeviceDecode(bytes) {
             i += length + 1;
         }
         // DOWNLINK RESPONSE
-        else if (channel_id === 0xfe) {
+        else if (channel_id === 0xfe || channel_id === 0xff) {
             result = handle_downlink_response(channel_type, bytes, i);
             decoded = Object.assign(decoded, result.data);
             i = result.offset;
