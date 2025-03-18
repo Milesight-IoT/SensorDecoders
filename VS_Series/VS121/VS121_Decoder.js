35dcdc148d8e71822fda87ae70054ac21104a041
Sway <sway@milesight.com>
2025-03-18 20:02:51 +0800

    fix(vs121): bug fix

--- VS_Series/VS121/VS121_Decoder.js
+++ VS_Series/VS121/VS121_Decoder.js
@@ -24,7 +24,7 @@ function Decoder(bytes, port) {
 function milesightDeviceDecode(bytes) {
     var decoded = {};
 
-    for (i = 0; i < bytes.length; ) {
+    for (var i = 0; i < bytes.length;) {
         var channel_id = bytes[i++];
         var channel_type = bytes[i++];
 
