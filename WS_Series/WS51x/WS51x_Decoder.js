c4c365b4412acd7117b508412f72e6438ce32ef4
Sway <sway@milesight.com>
2025-03-20 21:38:35 +0800

    feat(ws51x): support downlink response

--- WS_Series/WS51x/WS51x_Decoder.js
+++ WS_Series/WS51x/WS51x_Decoder.js
@@ -1,11 +1,11 @@
 /**
  * Payload Encoder
  *
- * Copyright 2024 Milesight IoT
+ * Copyright 2025 Milesight IoT
  *
  * @product WS51x
  */
-var RAW_VALUE = 0x01;
+var RAW_VALUE = 0x00;
 
 // Chirpstack v4
 function decodeUplink(input) {
@@ -26,7 +26,7 @@ function Decoder(bytes, port) {
 function milesightDeviceDecode(bytes) {
     var decoded = {};
 
-    for (var i = 0; i < bytes.length; ) {
+    for (var i = 0; i < bytes.length;) {
         var channel_id = bytes[i++];
         var channel_type = bytes[i++];
 
@@ -121,9 +121,16 @@ function milesightDeviceDecode(bytes) {
         // TEMPERATURE MUTATION ALARM (@since v1.9 @deprecated v2.1)
         else if (channel_id === 0x99 && channel_type === 0x67) {
             decoded.temperature = readInt16LE(bytes.slice(i, i + 2)) / 10;
-            decoded.temperature_mutation = readInt16LE(bytes.slice(i + 2, i + 4)) / 10;
+            decoded.temperature_mutation =
+                readInt16LE(bytes.slice(i + 2, i + 4)) / 10;
             decoded.temperature_alarm = readTemperatureAlarm(3);
             i += 5;
+        }
+        // DOWNLINK RESPONSE
+        else if (channel_id === 0xfe || channel_id === 0xff) {
+            var result = handle_downlink_response(channel_type, bytes, i);
+            decoded = Object.assign(decoded, result.data);
+            i = result.offset;
         } else {
             break;
         }
@@ -131,42 +138,107 @@ function milesightDeviceDecode(bytes) {
     return decoded;
 }
 
-function readUInt8(bytes) {
-    return bytes & 0xff;
+function handle_downlink_response(channel_type, bytes, offset) {
+    var decoded = {};
+
+    switch (channel_type) {
+        case 0x03:
+            decoded.report_interval = readUInt16LE(bytes.slice(offset, offset + 2));
+            offset += 2;
+            break;
+        case 0x06:
+            var data = readUInt8(bytes[offset]);
+            decoded.temperature_alarm_config = {};
+            decoded.temperature_alarm_config.condition = readConditionType(data & 0x07);
+            decoded.temperature_alarm_config.min_threshold = readInt16LE(bytes.slice(offset + 1, offset + 3)) / 10;
+            decoded.temperature_alarm_config.max_threshold = readInt16LE(bytes.slice(offset + 3, offset + 5)) / 10;
+            decoded.temperature_alarm_config.alarm_interval = readUInt16LE(bytes.slice(offset + 5, offset + 7));
+            decoded.temperature_alarm_config.alarm_times = readUInt16LE(bytes.slice(offset + 7, offset + 9));
+            offset += 9;
+            break;
+        case 0x10:
+            decoded.reboot = readYesNoStatus(1);
+            offset += 1;
+            break;
+        case 0x22:
+            decoded.delay_task = {};
+            decoded.delay_task.frame_count = readUInt8(bytes[offset]);
+            decoded.delay_task.delay_time = readUInt16LE(bytes.slice(offset + 1, offset + 3));
+            var data = readUInt8(bytes[offset + 3]);
+            var switch_bit_offset = { socket_status: 0 };
+            for (var key in switch_bit_offset) {
+                if ((data >>> (switch_bit_offset[key] + 4)) & 0x01) {
+                    decoded.delay_task[key] = readSocketStatus((data >> switch_bit_offset[key]) & 0x01);
+                }
+            }
+            offset += 4;
+            break;
+        case 0x23:
+            decoded.cancel_delay_task = readUInt8(bytes[offset]);
+            offset += 2;
+            break;
+        case 0x24:
+            decoded.current_threshold_config = {};
+            decoded.current_threshold_config.enable = readEnableStatus(bytes[offset]);
+            decoded.current_threshold_config.threshold = readUInt8(bytes[offset + 1]);
+            offset += 2;
+            break;
+        case 0x25:
+            var data = readUInt16LE(bytes.slice(offset, offset + 2));
+            decoded.child_lock_config = {};
+            decoded.child_lock_config.enable = readEnableStatus((data >>> 15) & 0x01);
+            decoded.child_lock_config.lock_time = data & 0x7fff;
+            offset += 2;
+            break;
+        case 0x26:
+            decoded.power_consumption_enable = readEnableStatus(bytes[offset]);
+            offset += 1;
+            break;
+        case 0x27:
+            decoded.reset_power_consumption = readYesNoStatus(1);
+            offset += 1;
+            break;
+        case 0x28:
+            decoded.report_status = readYesNoStatus(1);
+            offset += 1;
+            break;
+        case 0x2f:
+            decoded.led_enable = readEnableStatus(readUInt16LE(bytes.slice(offset, offset + 2)));
+            offset += 2;
+            break;
+        case 0x30:
+            decoded.over_current_protection = {};
+            decoded.over_current_protection.enable = readEnableStatus(bytes[offset]);
+            decoded.over_current_protection.trip_current = readUInt8(bytes[offset + 1]);
+            offset += 2;
+            break;
+        case 0x34:
+            // skip first byte
+            decoded.d2d_command = readD2DCommand(bytes.slice(offset + 1, offset + 3));
+            offset += 3;
+            break;
+        case 0x8d:
+            decoded.overload_current_protection = {};
+            decoded.overload_current_protection.enable = readEnableStatus(bytes[offset]);
+            offset += 1;
+            break;
+        case 0xa5:
+            decoded.led_reserve = readEnableStatus(bytes[offset]);
+            offset += 1;
+            break;
+        case 0xab:
+            decoded.temperature_calibration_config = {};
+            decoded.temperature_calibration_config.enable = readEnableStatus(bytes[offset]);
+            decoded.temperature_calibration_config.temperature = readInt16LE(bytes.slice(offset + 1, offset + 3)) / 10;
+            offset += 3;
+            break;
+        default:
+            throw new Error("unknown downlink response");
+    }
+
+    return { data: decoded, offset: offset };
 }
 
-function readInt8(bytes) {
-    var ref = readUInt8(bytes);
-    return ref > 0x7f ? ref - 0x100 : ref;
-}
-
-function readUInt16LE(bytes) {
-    var value = (bytes[1] << 8) + bytes[0];
-    return value & 0xffff;
-}
-
-function readInt16LE(bytes) {
-    var ref = readUInt16LE(bytes);
-    return ref > 0x7fff ? ref - 0x10000 : ref;
-}
-
-function readUInt16LE(bytes) {
-    var value = (bytes[1] << 8) + bytes[0];
-    return value & 0xffff;
-}
-
-function readUInt32LE(bytes) {
-    var value = (bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0];
-    return (value & 0xffffffff) >>> 0;
-}
-
-function getValue(map, key) {
-    if (RAW_VALUE) return key;
-
-    var value = map[key];
-    if (!value) value = "unknown";
-    return value;
-}
 
 function readProtocolVersion(bytes) {
     var major = (bytes & 0xf0) >> 4;
@@ -211,26 +283,27 @@ function readLoRaWANClass(type) {
 }
 
 function readResetEvent(status) {
-    var status_map = {
-        0: "normal",
-        1: "reset",
-    };
+    var status_map = { 0: "normal", 1: "reset" };
     return getValue(status_map, status);
 }
 
 function readDeviceStatus(status) {
-    var status_map = {
-        0: "off",
-        1: "on",
-    };
+    var status_map = { 0: "off", 1: "on" };
     return getValue(status_map, status);
 }
 
 function readSocketStatus(status) {
-    var status_map = {
-        0: "off",
-        1: "on",
-    };
+    var status_map = { 0: "off", 1: "on" };
+    return getValue(status_map, status);
+}
+
+function readEnableStatus(status) {
+    var status_map = { 0: "disable", 1: "enable" };
+    return getValue(status_map, status);
+}
+
+function readYesNoStatus(status) {
+    var status_map = { 0: "no", 1: "yes" };
     return getValue(status_map, status);
 }
 
@@ -251,4 +324,83 @@ function readTemperatureAlarm(type) {
         3: "mutation alarm",
     };
     return getValue(status_map, type);
+}
+
+function readConditionType(type) {
+    var condition_map = { 0: "disable", 1: "below", 2: "above", 3: "between", 4: "outside" };
+    return getValue(condition_map, type);
+}
+
+function readD2DCommand(bytes) {
+    return ("0" + (bytes[1] & 0xff).toString(16)).slice(-2) + ("0" + (bytes[0] & 0xff).toString(16)).slice(-2);
+}
+
+function readUInt8(bytes) {
+    return bytes & 0xff;
+}
+
+function readInt8(bytes) {
+    var ref = readUInt8(bytes);
+    return ref > 0x7f ? ref - 0x100 : ref;
+}
+
+function readUInt16LE(bytes) {
+    var value = (bytes[1] << 8) + bytes[0];
+    return value & 0xffff;
+}
+
+function readInt16LE(bytes) {
+    var ref = readUInt16LE(bytes);
+    return ref > 0x7fff ? ref - 0x10000 : ref;
+}
+
+function readUInt32LE(bytes) {
+    var value = (bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0];
+    return (value & 0xffffffff) >>> 0;
+}
+
+function getValue(map, key) {
+    if (RAW_VALUE) return key;
+
+    var value = map[key];
+    if (!value) value = "unknown";
+    return value;
+}
+
+if (!Object.assign) {
+    Object.defineProperty(Object, "assign", {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function (target) {
+            "use strict";
+            if (target == null) {
+                throw new TypeError("Cannot convert first argument to object");
+            }
+
+            var to = Object(target);
+            for (var i = 1; i < arguments.length; i++) {
+                var nextSource = arguments[i];
+                if (nextSource == null) {
+                    continue;
+                }
+                nextSource = Object(nextSource);
+
+                var keysArray = Object.keys(Object(nextSource));
+                for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+                    var nextKey = keysArray[nextIndex];
+                    var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+                    if (desc !== undefined && desc.enumerable) {
+                        // concat array
+                        if (Array.isArray(to[nextKey]) && Array.isArray(nextSource[nextKey])) {
+                            to[nextKey] = to[nextKey].concat(nextSource[nextKey]);
+                        } else {
+                            to[nextKey] = nextSource[nextKey];
+                        }
+                    }
+                }
+            }
+            return to;
+        },
+    });
 }
