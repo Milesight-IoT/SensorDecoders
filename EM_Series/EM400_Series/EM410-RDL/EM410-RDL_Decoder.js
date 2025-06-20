a9545d65e1f888f15418b4f5dd9ad4153089abf5
Sway <sway@milesight.com>
2025-06-20 15:21:52 +0800

    fix(em410-rdl): eslint

--- EM_Series/EM400_Series/EM410-RDL/EM410-RDL_Decoder.js
+++ EM_Series/EM400_Series/EM410-RDL/EM410-RDL_Decoder.js
@@ -7,6 +7,8 @@
  */
 var RAW_VALUE = 0x00;
 
+/* eslint no-redeclare: "off" */
+/* eslint-disable */
 // Chirpstack v4
 function decodeUplink(input) {
     var decoded = milesightDeviceDecode(input.bytes);
@@ -22,6 +24,7 @@ function Decode(fPort, bytes) {
 function Decoder(bytes, port) {
     return milesightDeviceDecode(bytes);
 }
+/* eslint-enable */
 
 function milesightDeviceDecode(bytes) {
     var decoded = {};
@@ -180,7 +183,7 @@ function milesightDeviceDecode(bytes) {
         }
         // DOWNLINK RESPONSE
         else if (channel_id === 0xfe || channel_id === 0xff) {
-            result = handle_downlink_response(channel_type, bytes, i);
+            var result = handle_downlink_response(channel_type, bytes, i);
             decoded = Object.assign(decoded, result.data);
             i = result.offset;
         }
@@ -565,6 +568,7 @@ function readConditionType(condition) {
     return getValue(condition_map, condition);
 }
 
+/* eslint-disable */
 function readUInt8(bytes) {
     return bytes & 0xff;
 }
