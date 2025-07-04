95c5d7444e938019c6a96a9b61c016a130fb5401
Sway <sway@milesight.com>
2025-07-04 13:43:57 +0800

    feat(em400-tld): ToF Laser Distance Sensor

---
+++ EM_Series/EM400_Series/EM400-TLD/EM400-TLD_Decoder.js
@@ -0,0 +1,80 @@
+/**
+ * Payload Decoder
+ *
+ * Copyright 2024 Milesight IoT
+ *
+ * @product EM400-TLD
+ */
+// Chirpstack v4
+function decodeUplink(input) {
+    var decoded = milesightDeviceDecode(input.bytes);
+    return { data: decoded };
+}
+
+// Chirpstack v3
+function Decode(fPort, bytes) {
+    return milesightDeviceDecode(bytes);
+}
+
+// The Things Network
+function Decoder(bytes, port) {
+    return milesightDeviceDecode(bytes);
+}
+
+function milesightDeviceDecode(bytes) {
+    var decoded = {};
+
+    for (var i = 0; i < bytes.length; ) {
+        var channel_id = bytes[i++];
+        var channel_type = bytes[i++];
+        // BATTERY
+        if (channel_id === 0x01 && channel_type === 0x75) {
+            decoded.battery = bytes[i];
+            i += 1;
+        }
+        // TEMPERATURE
+        else if (channel_id === 0x03 && channel_type === 0x67) {
+            decoded.temperature = readInt16LE(bytes.slice(i, i + 2)) / 10;
+            i += 2;
+        }
+        // DISTANCE
+        else if (channel_id === 0x04 && channel_type === 0x82) {
+            decoded.distance = readUInt16LE(bytes.slice(i, i + 2));
+            i += 2;
+        }
+        // POSITION
+        else if (channel_id === 0x05 && channel_type === 0x00) {
+            decoded.position = bytes[i] === 0 ? "normal" : "tilt";
+            i += 1;
+        }
+        // TEMPERATURE WITH ABNORMAL
+        else if (channel_id === 0x83 && channel_type === 0x67) {
+            decoded.temperature = readInt16LE(bytes.slice(i, i + 2)) / 10;
+            decoded.temperature_abnormal = bytes[i + 2] == 0 ? false : true;
+            i += 3;
+        }
+        // DISTANCE WITH ALARMING
+        else if (channel_id === 0x84 && channel_type === 0x82) {
+            decoded.distance = readUInt16LE(bytes.slice(i, i + 2));
+            decoded.distance_alarming = bytes[i + 2] == 0 ? false : true;
+            i += 3;
+        } else {
+            break;
+        }
+    }
+
+    return decoded;
+}
+
+/* ******************************************
+ * bytes to number
+ ********************************************/
+function readUInt16LE(bytes) {
+    var value = (bytes[1] << 8) + bytes[0];
+    return value & 0xffff;
+}
+
+function readInt16LE(bytes) {
+    var ref = readUInt16LE(bytes);
+    return ref > 0x7fff ? ref - 0x10000 : ref;
+}
