2768de39a86785b331529c9afa9f63b0d1cdbb45
Sway <sway@milesight.com>
2025-07-04 14:21:23 +0800

    feat(em500-pp): Pipe Pressure Sensor

---
+++ EM_Series/EM500_Series/EM500-PP/EM500-PP_Decoder.js
@@ -0,0 +1,74 @@
+/**
+ * Payload Decoder
+ *
+ * Copyright 2024 Milesight IoT
+ *
+ * @product EM500-PP
+ */
+// Chirpstack v4
+function decodeUplink(input) {
+    var decoded = milesightDeviceDecode(input.bytes);
+    return { data: decoded };
+}
+
+// Chirpstack v3
+function Decode(fPort, bytes) {
+    return milesightDeviceDecode(bytes);
+}
+
+// The Things Network
+function Decoder(bytes, port) {
+    return milesightDeviceDecode(bytes);
+}
+
+function milesightDeviceDecode(bytes) {
+    var decoded = {};
+
+    for (var i = 0; i < bytes.length; ) {
+        var channel_id = bytes[i++];
+        var channel_type = bytes[i++];
+
+        // BATTERY
+        if (channel_id === 0x01 && channel_type === 0x75) {
+            decoded.battery = bytes[i];
+            i += 1;
+        }
+        // PRESSURE
+        else if (channel_id === 0x03 && channel_type === 0x7b) {
+            decoded.pressure = readInt16LE(bytes.slice(i, i + 2));
+            i += 2;
+        }
+        // HISTROY DATA
+        else if (channel_id === 0x20 && channel_type === 0xce) {
+            var point = {};
+            point.timestamp = readUInt32LE(bytes.slice(i, i + 4));
+            point.pressure = readInt16LE(bytes.slice(i + 4, i + 6));
+
+            decoded.history = decoded.history || [];
+            decoded.history.push(point);
+            i += 6;
+        } else {
+            break;
+        }
+    }
+
+    return decoded;
+}
+
+/* ******************************************
+ * bytes to number
+ ********************************************/
+function readUInt16LE(bytes) {
+    var value = (bytes[1] << 8) + bytes[0];
+    return value & 0xffff;
+}
+
+function readInt16LE(bytes) {
+    var ref = readUInt16LE(bytes);
+    return ref > 0x7fff ? ref - 0x10000 : ref;
+}
+
+function readUInt32LE(bytes) {
+    var value = (bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0];
+    return value & 0xffffffff;
+}
