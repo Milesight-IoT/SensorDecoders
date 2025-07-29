0875bbf87f4ec3df031178a12ef2b345d35da7fc
Sway <sway@milesight.com>
2025-07-29 14:01:53 +0800

    fix: bugs

--- EM_Series/EM500_Series/EM500-PP/EM500-PP_Decoder.js
+++ EM_Series/EM500_Series/EM500-PP/EM500-PP_Decoder.js
@@ -29,7 +29,7 @@ function Decoder(bytes, port) {
 function milesightDeviceDecode(bytes) {
     var decoded = {};
 
-    for (var i = 0; i < bytes.length;) {
+    for (var i = 0; i < bytes.length; ) {
         var channel_id = bytes[i++];
         var channel_type = bytes[i++];
 
@@ -145,7 +145,7 @@ function handle_downlink_response(channel_type, bytes, offset) {
             offset += 4;
             break;
         case 0x17:
-            decoded.time_zone = readInt16LE(bytes.slice(offset, offset + 2));
+            decoded.time_zone = readTimeZone(readInt16LE(bytes.slice(offset, offset + 2)));
             offset += 2;
             break;
         case 0x1c:
@@ -209,10 +209,10 @@ function handle_downlink_response(channel_type, bytes, offset) {
             decoded.d2d_master_config.push(d2d_master_config);
             break;
         case 0xf1:
-            decoded.pressure_calibration_config = {};
+            decoded.pressure_calibration_settings = {};
             // skip 1 byte
-            decoded.pressure_calibration_config.enable = readEnableStatus(bytes[offset + 1]);
-            decoded.pressure_calibration_config.calibration_value = readInt16LE(bytes.slice(offset + 2, offset + 4));
+            decoded.pressure_calibration_settings.enable = readEnableStatus(bytes[offset + 1]);
+            decoded.pressure_calibration_settings.calibration_value = readInt16LE(bytes.slice(offset + 2, offset + 4));
             offset += 4;
             break;
         case 0xf2:
@@ -220,7 +220,7 @@ function handle_downlink_response(channel_type, bytes, offset) {
             offset += 2;
             break;
         case 0xf5:
-            decoded.threshold_alarm_release_enable = readEnableStatus(bytes[offset]);
+            decoded.alarm_release_enable = readEnableStatus(bytes[offset]);
             offset += 1;
             break;
         default:
@@ -290,6 +290,11 @@ function readYesNoStatus(status) {
 function readEnableStatus(status) {
     var status_map = { 0: "disable", 1: "enable" };
     return getValue(status_map, status);
+}
+
+function readTimeZone(time_zone) {
+    var timezone_map = { "-120": "UTC-12", "-110": "UTC-11", "-100": "UTC-10", "-95": "UTC-9:30", "-90": "UTC-9", "-80": "UTC-8", "-70": "UTC-7", "-60": "UTC-6", "-50": "UTC-5", "-40": "UTC-4", "-35": "UTC-3:30", "-30": "UTC-3", "-20": "UTC-2", "-10": "UTC-1", 0: "UTC", 10: "UTC+1", 20: "UTC+2", 30: "UTC+3", 35: "UTC+3:30", 40: "UTC+4", 45: "UTC+4:30", 50: "UTC+5", 55: "UTC+5:30", 57: "UTC+5:45", 60: "UTC+6", 65: "UTC+6:30", 70: "UTC+7", 80: "UTC+8", 90: "UTC+9", 95: "UTC+9:30", 100: "UTC+10", 105: "UTC+10:30", 110: "UTC+11", 120: "UTC+12", 127: "UTC+12:45", 130: "UTC+13", 140: "UTC+14" };
+    return getValue(timezone_map, time_zone);
 }
 
 function readConditionType(condition) {
