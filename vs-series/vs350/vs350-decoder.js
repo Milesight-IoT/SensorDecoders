11caf551b6ae4663bd9f6a3c68767244911b7f8b
QDFish <qdfishyooooooh@gmail.com>
2025-11-10 15:26:46 +0800

    bug: fix

--- vs-series/vs350/vs350-decoder.js
+++ vs-series/vs350/vs350-decoder.js
@@ -507,7 +507,7 @@ function getValue(map, key) {
     return value;
 }
 
-if (!Object.assign) {
+//if (!Object.assign) {
     Object.defineProperty(Object, "assign", {
         enumerable: false,
         configurable: true,
@@ -543,4 +543,4 @@ if (!Object.assign) {
             return to;
         },
     });
-}
+//}
