05f9b18bf4da82cfdc6b36559bbf49cc78bef04e
QDFish <qdfishyooooooh@gmail.com>
2025-11-10 15:25:41 +0800

    bug: fix

--- ts-series/ts101/ts101-decoder.js
+++ ts-series/ts101/ts101-decoder.js
@@ -322,7 +322,7 @@ function getValue(map, key) {
     return value;
 }
 
-if (!Object.assign) {
+//if (!Object.assign) {
     Object.defineProperty(Object, "assign", {
         enumerable: false,
         configurable: true,
@@ -358,4 +358,4 @@ if (!Object.assign) {
             return to;
         },
     });
-}
+//}
