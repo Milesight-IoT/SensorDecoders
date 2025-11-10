d46a52762a4ae9fe656987d5fde1c229d5c66d37
QDFish <qdfishyooooooh@gmail.com>
2025-11-10 16:01:28 +0800

    bug: fix

--- em-series/em500-smtc/em500-smtc-decoder.js
+++ em-series/em500-smtc/em500-smtc-decoder.js
@@ -667,7 +667,7 @@ function getValue(map, key) {
     return value;
 }
 
-if (!Object.assign) {
+//if (!Object.assign) {
     Object.defineProperty(Object, "assign", {
         enumerable: false,
         configurable: true,
@@ -703,4 +703,4 @@ if (!Object.assign) {
             return to;
         },
     });
-}
+//}
