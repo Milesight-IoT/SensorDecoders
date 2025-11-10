10932765338a424ab90d757d3405b561d16e2561
QDFish <qdfishyooooooh@gmail.com>
2025-11-10 16:00:44 +0800

    bug: fix

--- em-series/em500-series/em500-co2/em500-co2-decoder.js
+++ em-series/em500-series/em500-co2/em500-co2-decoder.js
@@ -510,7 +510,7 @@ function getValue(map, key) {
     return value;
 }
 
-if (!Object.assign) {
+//if (!Object.assign) {
     Object.defineProperty(Object, "assign", {
         enumerable: false,
         configurable: true,
@@ -546,4 +546,4 @@ if (!Object.assign) {
             return to;
         },
     });
-}
+//}
