a32f60aed440a2600528b756702bed842821f4ea
QDFish <qdfishyooooooh@gmail.com>
2025-11-10 15:25:45 +0800

    bug: fix

--- ts-series/ts201-v2/ts201-v2-decoder.js
+++ ts-series/ts201-v2/ts201-v2-decoder.js
@@ -608,7 +608,7 @@ function getValue(map, key) {
     return value;
 }
 
-if (!Object.assign) {
+//if (!Object.assign) {
     Object.defineProperty(Object, "assign", {
         enumerable: false,
         configurable: true,
@@ -644,4 +644,4 @@ if (!Object.assign) {
             return to;
         },
     });
-}
+//}
