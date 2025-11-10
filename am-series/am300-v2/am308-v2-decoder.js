04975a12a1c80d1c678d9dcb3a5ce8c55b3c7b5a
QDFish <qdfishyooooooh@gmail.com>
2025-11-10 16:01:11 +0800

    bug: fix

--- am-series/am300-v2/am308-v2-decoder.js
+++ am-series/am300-v2/am308-v2-decoder.js
@@ -445,7 +445,7 @@ function getValue(map, key) {
     return value;
 }
 
-if (!Object.assign) {
+//if (!Object.assign) {
     Object.defineProperty(Object, "assign", {
         enumerable: false,
         configurable: true,
@@ -481,4 +481,4 @@ if (!Object.assign) {
             return to;
         },
     });
-}
+//}
--- am-series/am300-v2/am319-hcho-v2-decoder.js
+++ am-series/am300-v2/am319-hcho-v2-decoder.js
@@ -454,7 +454,7 @@ function getValue(map, key) {
     return value;
 }
 
-if (!Object.assign) {
+//if (!Object.assign) {
     Object.defineProperty(Object, "assign", {
         enumerable: false,
         configurable: true,
@@ -490,4 +490,4 @@ if (!Object.assign) {
             return to;
         },
     });
-}
+//}
