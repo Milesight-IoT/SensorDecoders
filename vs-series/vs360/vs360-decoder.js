55d828388810cf344c7770243ef982a133c10c5b
QDFish <qdfishyooooooh@gmail.com>
2025-11-10 16:00:48 +0800

    bug: fix

--- vs-series/vs360/vs360-decoder.js
+++ vs-series/vs360/vs360-decoder.js
@@ -550,7 +550,7 @@ function getValue(map, key) {
     return value;
 }
 
-if (!Object.assign) {
+//if (!Object.assign) {
     Object.defineProperty(Object, "assign", {
         enumerable: false,
         configurable: true,
@@ -586,5 +586,5 @@ if (!Object.assign) {
             return to;
         },
     });
-}
+//}
 
