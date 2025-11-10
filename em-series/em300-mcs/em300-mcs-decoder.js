20146483a563742f61c02b04ec666b13644c1add
QDFish <qdfishyooooooh@gmail.com>
2025-11-10 15:24:17 +0800

    bug: fix

--- em-series/em300-mcs/em300-mcs-decoder.js
+++ em-series/em300-mcs/em300-mcs-decoder.js
@@ -334,7 +334,7 @@ function getValue(map, key) {
     return value;
 }
 
-if (!Object.assign) {
+//if (!Object.assign) {
     Object.defineProperty(Object, "assign", {
         enumerable: false,
         configurable: true,
@@ -370,4 +370,4 @@ if (!Object.assign) {
             return to;
         },
     });
-}
+//}
