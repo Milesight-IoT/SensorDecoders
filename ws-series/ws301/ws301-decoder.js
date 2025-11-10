97570fa144c90e6fec62bcd67a1b53e1e297aa05
QDFish <qdfishyooooooh@gmail.com>
2025-11-10 16:00:57 +0800

    bug: fix

--- ws-series/ws301/ws301-decoder.js
+++ ws-series/ws301/ws301-decoder.js
@@ -316,7 +316,7 @@ function getValue(map, key) {
     return value;
 }
 
-if (!Object.assign) {
+//if (!Object.assign) {
     Object.defineProperty(Object, "assign", {
         enumerable: false,
         configurable: true,
@@ -352,4 +352,4 @@ if (!Object.assign) {
             return to;
         },
     });
-}
+//}
