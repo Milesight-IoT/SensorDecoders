bc1f55f178b805909e6123dbc971e8d916cf3dac
QDFish <qdfishyooooooh@gmail.com>
2025-11-10 16:01:36 +0800

    bug: fix

--- am-series/am308l/am308l-decoder.js
+++ am-series/am308l/am308l-decoder.js
@@ -447,7 +447,7 @@ function getValue(map, key) {
     return value;
 }
 
-if (!Object.assign) {
+//if (!Object.assign) {
     Object.defineProperty(Object, "assign", {
         enumerable: false,
         configurable: true,
@@ -483,4 +483,4 @@ if (!Object.assign) {
             return to;
         },
     });
-}
+//}
