e28ce661080e314ccb45b92583a4cc70a604393f
QDFish <qdfishyooooooh@gmail.com>
2025-11-10 15:27:16 +0800

    bug: fix

--- ws-series/ws502-cn/ws502-cn-decoder.js
+++ ws-series/ws502-cn/ws502-cn-decoder.js
@@ -396,7 +396,7 @@ function getValue(map, key) {
     return value;
 }
 
-if (!Object.assign) {
+//if (!Object.assign) {
     Object.defineProperty(Object, "assign", {
         enumerable: false,
         configurable: true,
@@ -432,4 +432,4 @@ if (!Object.assign) {
             return to;
         },
     });
-}
+//}
