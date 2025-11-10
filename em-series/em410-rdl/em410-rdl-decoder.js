622c0718ac117a4de45fa50d5eb9d942946b15be
QDFish <qdfishyooooooh@gmail.com>
2025-11-10 15:24:56 +0800

    bug: fix

--- em-series/em410-rdl/em410-rdl-decoder.js
+++ em-series/em410-rdl/em410-rdl-decoder.js
@@ -567,7 +567,7 @@ function getValue(map, key) {
     return value;
 }
 
-if (!Object.assign) {
+//if (!Object.assign) {
     Object.defineProperty(Object, "assign", {
         enumerable: false,
         configurable: true,
@@ -603,4 +603,4 @@ if (!Object.assign) {
             return to;
         },
     });
-}
+//}
