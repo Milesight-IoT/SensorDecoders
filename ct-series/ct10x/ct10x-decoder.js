2b77c64279bde69581667cd5564475dc10e5a9b1
QDFish <qdfishyooooooh@gmail.com>
2025-11-10 16:01:21 +0800

    bug: fix

--- ct-series/ct10x/ct10x-decoder.js
+++ ct-series/ct10x/ct10x-decoder.js
@@ -315,7 +315,7 @@ function getValue(map, key) {
     return value;
 }
 
-if (!Object.assign) {
+//if (!Object.assign) {
     Object.defineProperty(Object, "assign", {
         enumerable: false,
         configurable: true,
@@ -351,4 +351,4 @@ if (!Object.assign) {
             return to;
         },
     });
-}
+//}
